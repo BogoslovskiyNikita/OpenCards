7a8d45df1bf37d0b24fa1d9f5a4f0a582266d0ea
BogoslovskiyNikita <65765163+BogoslovskiyNikita@users.noreply.github.com>
2023-11-29 18:00:11 +0600

    [feature] CategoryNameLineEdit clears after adding new category

--- entities/ui_elements/new_category_pop_up/new_category_pop_up.gd
+++ entities/ui_elements/new_category_pop_up/new_category_pop_up.gd
@@ -5,10 +5,11 @@ func open():
 
 
 func close():
+	$"%CategoryNameLineEdit".clear()
 	hide()
 
 
 func _on_AddButton_pressed():
 	DatabaseManager.add_category($"%CategoryNameLineEdit".text)
 	## TODO: Check for DB errors and uniques category name here.
-	hide()
+	close()
