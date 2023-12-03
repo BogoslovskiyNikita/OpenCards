bf424fb645ea6b284ef5ac0f402ba0ba6b7cb552
BogoslovskiyNikita <65765163+BogoslovskiyNikita@users.noreply.github.com>
2023-12-03 18:18:29 +0600

    Added description on card swipe mechanism

--- entities/ui_elements/learning_card/learning_card.gd
+++ entities/ui_elements/learning_card/learning_card.gd
@@ -1,4 +1,9 @@
 extends Control
 
+"""Card swipe mechanism: change rotation degree while card is
+moving. If card x_offset above some threshold, card count as swiped"""
+
+var threshold: int
+
 func _ready():
 	pass
