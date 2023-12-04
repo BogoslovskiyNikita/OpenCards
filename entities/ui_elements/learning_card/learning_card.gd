ed6a7b8fdf9be272192bfd73fa7612686bc811be
BogoslovskiyNikita <65765163+BogoslovskiyNikita@users.noreply.github.com>
2023-12-04 20:33:45 +0600

    [feature] Added flip animation to card

--- entities/ui_elements/learning_card/learning_card.gd
+++ entities/ui_elements/learning_card/learning_card.gd
@@ -5,5 +5,20 @@ moving. If card x_offset above some threshold, card count as swiped"""
 
 var threshold: int
 
+var flipped = false
+
 func _ready():
 	pass
+
+
+func _on_Panel_gui_input(event: InputEvent):
+	if event is InputEventMouseButton and event.is_pressed():
+		flip()
+
+
+func flip():
+	if flipped:
+		return
+	
+	$"%AnimationPlayer".play("flip")
+	flipped = true
