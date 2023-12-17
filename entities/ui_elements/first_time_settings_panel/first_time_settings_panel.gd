a47752cbe24d0d8bd47aeb4df7f0427395d56813
BogoslovskiyNikita <65765163+BogoslovskiyNikita@users.noreply.github.com>
2023-12-18 00:37:20 +0600

    [fix] Closing settings panel on "Save" button

--- entities/ui_elements/first_time_settings_panel/first_time_settings_panel.gd
+++ entities/ui_elements/first_time_settings_panel/first_time_settings_panel.gd
@@ -18,9 +18,10 @@ func _ready():
 		$"%LanguageToLearnOptionButton".add_item(language_name)
 	
 	$"%LanguageToLearnOptionButton".select(1)
-
+	
 
 func _on_SaveButton_pressed():
 	Settings.set_language_to_learn($"%LanguageToLearnOptionButton".get_item_text($"%LanguageToLearnOptionButton".get_selected_id()))
 	Settings.set_ui_language($"%UILanguageOptionButton".get_item_text($"%UILanguageOptionButton".get_selected_id()))
 	Settings.set_native_language($"%NativeLanguageOptionButton".get_item_text($"%NativeLanguageOptionButton".get_selected_id()))
+	hide()
