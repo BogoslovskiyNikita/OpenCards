82db18947289c4e3cc5ff115c92a938f424d1416
BogoslovskiyNikita <65765163+BogoslovskiyNikita@users.noreply.github.com>
2023-12-22 23:41:16 +0600

    Added DescriptionFinder singletone

---
+++ entities/description_finder.gd
@@ -0,0 +1,41 @@
+extends Node
+
+const LanguageCodes = {
+	"Russian": "ru",
+	"English": "en",
+}
+
+var client := SimpleHTTPClient.new()
+var url := "https://api.dictionaryapi.dev/api/v2/entries/en/%s"
+
+signal description_found(descriptions)
+
+func find_description(query: String):
+	if client.requesting:
+		return
+	
+	var response: HTTPResponse = yield(
+		client.request_async(
+			url % query,
+			{},
+			true,
+			HTTPClient.METHOD_GET),
+			"completed"
+			)
+	if response.successful():
+		var result = parse_json(response.body.get_string_from_utf8())
+		result = result[0]["meanings"][0]["definitions"][0]["definition"]
+		if result:
+			emit_signal("description_found", result)
+	else:
+		print(url % query)
+		print(response.code)
+		print(response.body.get_string_from_utf8())
+
+func _ready():
+	connect("description_found", self, "test")
+	find_description('something')
+
+
+func test(descriptions):
+	print(descriptions)
